ea95aad6f5390e63700117961d647a4326affdfc
Nethaji <nethajinandhu007@gmail.com>
2022-03-05 13:56:22 +0530

    Javascript assignment-3 submission

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,18 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
-    // Write you code here
+    switch(true){
+        case(!hasDrivingLiscence && !isTired && isSober):
+            console.log("You cannot Drive");
+            break;
+        case(hasDrivingLiscence && !isTired && isSober):
+            console.log("you can Drive");
+            break;
+        case(hasDrivingLiscence && isTired && !isSober):
+            console.log("You should't Drive");
+            break;
+        default:
+        console.log("You Cannot be Tired and Sober at the same time ! or You left some details to be filled !");
+        
+    }
 }
 
 module.exports = CanDrive;
