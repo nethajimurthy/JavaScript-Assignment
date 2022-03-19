89b4d0fc547025010469ed190bacb81986f47040
unknown <nethajinandhu007@gmail.com>
2022-03-19 13:43:23 +0530

    todolist with API assignment submission

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,41 +1,46 @@
-let textbox=document.querySelector("#input_box");
 let sbutton= document.querySelector("#subbutton");
 let todos= document.querySelector(".todos");
-let key = 0;
-sbutton.onclick=(e=> {
-    if(textbox.value !== ""){
-    createtodo(textbox.value,key);
-    key++;
-    textbox.value="";
-    }
-    else{
-        alert("Cannot be empty box");
-    }
-});
 
-function createtodo(text,key){
+function createtodo(text,key,status){
     let todolist=document.createElement("ul");
-    todolist.setAttribute("id",key);
+    todolist.setAttribute("class","todolist");
+    let userid=document.createElement('p');
+    userid.setAttribute("id",'id');
+    userid.append("ID NO" +key);
     let val=document.createTextNode(text);
-    let del=document.createElement('button');
-    del.setAttribute("class","delete_button");
-    del.append("delete");
-    del.setAttribute("id",key);
-    todolist.append(val,del);
+    let result=document.createElement('div');
+    result.setAttribute("id",'box');
+    result.append(status=="true"? 'completed':'Failed');
+    todolist.append(userid);
+    todolist.append(result);
+    todolist.append(val);
+    
     todos.appendChild(todolist);
-    del.onclick=((data)=>{
-        todolist.remove();
-    });
 
-    todolist.setAttribute("style","height: 33px;padding-left: 35px;border: 3px solid;border-color: skyblue;border-radius: 34px;background: gainsboro;text-align:left;padding-top: 7px;font-size: 22px;width: 486px;margin-left: -20px;");
+    todolist.setAttribute("style","border: 3px solid;border-color: skyblue;border-radius: 9px;background: gainsboro;padding-top: 7px;font-size: 22px;width: 486px;display:block;padding-left:0px;box-shadow:4px 5px darkslateblue");
 
-    del.setAttribute("style","height: 37px;width: 72px;display: inline;float:right;border-radius: 25px;background-color: crimson;margin-top: -6.2px;margin-right: 2px;");
-
+    userid.setAttribute('style',"background-color: darkorange;border-radius: 25px;padding-bottom: 3px;width: 100px;margin-top: -4px;text-align: center;float: left;margin-right:10px;");
+    
+    status=="true"? result.setAttribute('style',"background-color: forestgreen;border-radius: 25px;padding-bottom: 3px;width: 100px;margin-top: -4px;text-align: center;float: right;margin-bottom:inherit;"):result.setAttribute('style',"background-color: red;border-radius: 25px;padding-bottom: 3px;width: 100px;margin-top: -4px;text-align: center;float: right;margin-bottom:inherit;");
 }
 
 document.querySelector("body").setAttribute("style",
 "display: block;text-align: center;padding: 43px;margin: 31px;background-color: cadetblue;");
-textbox.setAttribute("style","height: 24px;width: 429px;border-color: cornflowerblue;border-radius: 13px;");
 sbutton.setAttribute("style","font-family: serif;color: unset;background: cornflowerblue;border-radius: 12px;");
 todos.setAttribute("style","text-align: center;padding: 26px;width: 500px;display: block;margin: auto;");
+document.getElementById('status').setAttribute('style',"width: 180px;height: 25px;border-radius: 12px;background-color: royalblue");
 
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(data=> data.json())
+.then(completedata=> {
+    sbutton.onclick=(e=> {
+        todos.innerHTML='';
+        let status=document.getElementById("status");
+        completedata.map((x)=> { 
+            if(status.value==x.completed.toString()){                    
+                createtodo(x.title,x.id,status.value)
+            }
+        })
+    })
+})
